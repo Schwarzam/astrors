42480c84512524f9ea3084809fd4be10fec4dc86
schwarzam <gustavo.b.schwarz@gmail.com>
2023-07-14 09:49:38 -0300

    first test

---
+++ build.rs
@@ -0,0 +1,12 @@
+use std::path::Path;
+
+fn main() {
+    
+    println!("cargo:rustc-link-search=lib/fitsio2");
+
+    cc::Build::new()
+        .file("lib/fits_hcompress.c")
+        .file("lib/fits_hdecompress.c")
+        .include("lib/fitsio2.h")
+        .compile("cfitsio");
+}
