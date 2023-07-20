53085475938ab2ab6c26a5693b949e18b7dca61f
schwarzam <gustavo.b.schwarz@gmail.com>
2023-07-19 22:55:34 -0300

    Implementing data types

--- src/io/aux.rs
+++ src/io/aux.rs
@@ -1,5 +1,39 @@
 use ndarray::{ArrayD, IxDyn};
 
+pub enum DataType {
+    Int8 = 8,
+    Int16 = 16,
+    Int32 = 32,
+    Int64 = 64,
+    Float32 = -32,
+    Float64 = -64
+}
+
+impl DataType {
+    pub fn nbytes(&self) -> usize {
+        match self {
+            DataType::Int8 => 1,    // 8 bits = 1 byte
+            DataType::Int16 => 2,   // 16 bits = 2 bytes
+            DataType::Int32 => 4,   // 32 bits = 4 bytes
+            DataType::Int64 => 8,   // 64 bits = 8 bytes
+            DataType::Float32 => 4, // 32 bits = 4 bytes
+            DataType::Float64 => 8, // 64 bits = 8 bytes
+        }
+    }
+
+    pub fn from_bitpix(bitpix: i32) -> Option<DataType> {
+        match bitpix {
+            8 => Some(DataType::Int8),
+            16 => Some(DataType::Int16),
+            32 => Some(DataType::Int32),
+            64 => Some(DataType::Int64),
+            -32 => Some(DataType::Float32),
+            -64 => Some(DataType::Float64),
+            _ => panic!("Unknown bitpix value"),
+        }
+    }
+}
+
 pub fn bytes_to_f32_vec(bytes: &[u8]) -> Vec<f32> {
     bytes
         .chunks(4)
--- src/io/image.rs
+++ src/io/image.rs
@@ -2,22 +2,10 @@ use std::fs::File;
 use std::io::Read;
 use crate::io::header::Header;
 
-use crate::io::aux::{bytes_to_f32_vec, vec_to_ndarray};
-
+use crate::io::aux::{bytes_to_f32_vec, vec_to_ndarray, DataType};
 use ndarray::{ArrayD, IxDyn};
 
-enum DataType {
-    Int8,
-    Int16,
-    Int32,
-    Int64,
-    UInt8,
-    UInt16,
-    UInt32,
-    UInt64,
-    Float32,
-    Float64,
-}
+ 
 
 pub struct Data {
     data: ndarray::ArrayD<DataType>,
@@ -28,12 +16,18 @@ impl Data {
     pub fn read_from_filebytes(f: &mut File, header: &mut Header) -> std::io::Result<()> {
         
         let naxis = header.get_value("NAXIS").unwrap().parse::<usize>().unwrap();
-
         let naxis1 = header.get_value("NAXIS1").unwrap().parse::<usize>().unwrap();
         let naxis2 = header.get_value("NAXIS2").unwrap().parse::<usize>().unwrap();
 
+        let bitpix : i32 = header.get_value("BITPIX").unwrap().parse::<i32>().unwrap();
 
-        let mut databuf = vec![0; naxis1 * naxis2 * 4];
+        // I want to check the bitpix value with the data type 
+
+        let dtype = DataType::from_bitpix(bitpix).unwrap();
+        let nbytes = dtype.nbytes();
+        println!("nbytes: {:?}", nbytes);
+
+        let mut databuf = vec![0; naxis1 * naxis2 * nbytes];
         
         let data = f.read(&mut databuf)?;
     
@@ -41,6 +35,7 @@ impl Data {
     
         let shape = vec![100, 100];
         let ndarray = vec_to_ndarray(vect, shape); 
+
 
         println!("{:?}", ndarray); 
         Ok(())
