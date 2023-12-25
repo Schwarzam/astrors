3a99304cdd67b57f750b5b2c020c139e8a92c27e
schwarzam <gustavo.b.schwarz@gmail.com>
2023-12-25 00:46:33 -0300

    making progress on code structure

--- tests/common.rs
+++ tests/common.rs
@@ -4,3 +4,7 @@ use std::path::{Path, PathBuf};
 pub fn get_testdata_path( filename : &str ) -> PathBuf {
     Path::new(env!("CARGO_MANIFEST_DIR")).join("tests").join("data").join(filename)
 }
+
+pub fn get_outtestdata_path( filename : &str ) -> PathBuf {
+    Path::new(env!("CARGO_MANIFEST_DIR")).join("tests").join("data").join("outputs").join(filename)
+}
--- tests/header_test.rs
+++ tests/header_test.rs
@@ -11,35 +11,43 @@ mod tests {
 
     #[test]
     fn read_header() -> Result<()> {
-        
         let testfile = common::get_testdata_path("WFPC2u57.fits");
-
         let mut f: File = File::open(testfile)?;
-
         let mut header = Header::new();
         header.read_from_file(&mut f)?;
         // header.pretty_print_advanced();
-
         Ok(())
     }
 
     #[test]
     fn read_write_header() -> Result<()> {
-        
         let testfile = common::get_testdata_path("WFPC2u57.fits");
-
         let mut f: File = File::open(testfile)?;
-
         let mut header = Header::new();
         header.read_from_file(&mut f)?;
         // header.pretty_print_advanced();
-        
-        let output_test = common::get_testdata_path("test.fits");
+        let output_test = common::get_outtestdata_path("header_outtest.fits");
         let mut outfile: File = File::create(output_test)?;
-
         header.write_to_buffer(&mut outfile)?;
         outfile.flush()?;
-
         Ok(())
     }
+
+    #[test]
+    fn header_modify_keyword() -> Result<()> {
+        let testfile = common::get_testdata_path("WFPC2u57.fits");
+        let mut f: File = File::open(testfile)?;
+        let mut header = Header::new();
+        header.read_from_file(&mut f)?;
+        // header.pretty_print_advanced();
+        header.modify_keyword("SIMPLE", "T");
+        header.get_card("CTYPE1").unwrap().set_value("TESTING");
+        
+
+
+        let output_test = common::get_outtestdata_path("header_modify_outtest.fits");
+        let mut outfile: File = File::create(output_test)?;
+        header.write_to_buffer(&mut outfile)?;
+        outfile.flush()?;
+        Ok(())
 }
