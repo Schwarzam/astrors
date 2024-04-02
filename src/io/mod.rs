0b77937d6d49d61b0c20b643953f0578101a7a64
Gustavo Schwarz <alberice.vincenzo@gmail.com>
2024-04-02 00:18:12 -0300

    added compression cfitsio c function signatures

--- src/io/mod.rs
+++ src/io/mod.rs
@@ -5,14 +5,39 @@ pub mod hdulist;
 
 pub use self::header::Header;
 
-use std::os::raw::c_char;
-extern {
+pub mod compression_algorithms {
+    use std::os::raw::c_char;
 
-    //https://github.com/Schwarzam/astrors/blob/2c67c1f76f3cf658f1811d773b87602f05e42573/old/src/main.rs
-    // Is a working example of how to use the fits_hcompress and fits_hdecompress functions
-
-    pub fn fits_hcompress(a: *mut i32, ny: i32, nx: i32, scale: i32, output: *mut c_char, nbytes: *mut i64, status: *mut i32) -> i32;
-    pub fn fits_hdecompress(input: *mut c_char, output: *mut i32, ny: i32, nx: i32, status: *mut i32) -> i32;
-
-    //TODO: Add signatures for all the other C functions
+    extern {
+        // ----- ricecomp.c -----
+        // - compress
+        pub fn fits_rcomp(a: *mut i32, nx: i32, c: *mut u8, clen: i32, nblock: i32) -> i32;
+        pub fn fits_rcomp_short(a: *mut i16, nx: i32, c: *mut u8, clen: i32, nblock: i32) -> i32;
+        pub fn fits_rcomp_byte(a: *mut i8, nx: i32, c: *mut u8, clen: i32, nblock: i32) -> i32;
+        // - decompress
+        pub fn fits_rdecomp(c: *mut u8, clen: i32, array: *mut u32, nx: i32, nblock: i32) -> i32;
+        pub fn fits_rdecomp_short(c: *mut u8, clen: i32, array: *mut u16, nx: i32, nblock: i32) -> i32;
+        pub fn fits_rdecomp_byte(c: *mut u8, clen: i32, array: *mut u8, nx: i32, nblock: i32) -> i32;
+        
+    
+        // ----- quantize.c -----
+        pub fn fits_quantize_float(row: i64, fdata: *mut f32, nxpix: i64, nypix: i64, nullcheck: i32, in_null_value: f32, qlevel: f32, dither_method: i32, idata: *mut i32, bscale: *mut f64, bzero: *mut f64, iminval: *mut i32, imaxval: *mut i32) -> i32;
+        
+        // ----- pliocomp.c -----
+        pub fn pl_p2li(pxsrc: *mut i32, xs: i32, lldst: *mut i16, npix: i32) -> i32;
+        pub fn pl_l2pi(ll_src: *mut i16, xs: i32, px_dst: *mut i32, npix: i32) -> i32;
+        
+        // ----- hcompress.c -----
+        pub fn fits_hcompress(a: *mut i32, ny: i32, nx: i32, scale: i32, output: *mut c_char, nbytes: *mut i64, status: *mut i32) -> i32;
+        pub fn fits_hcompress64(a: *mut i64, ny: i32, nx: i32, scale: i32, output: *mut c_char, nbytes: *mut i64, status: *mut i32) -> i32;
+        
+        // ----- hdecompress.c -----
+        pub fn fits_hdecompress64(input: *mut c_char, smooth: i32, a: *mut i64, ny: *mut i32, nx: *mut i32, scale: *mut i32, status: *mut i32) -> i32;
+        pub fn fits_hdecompress(input: *mut c_char, smooth: i32, a: *mut i32, ny: *mut i32, nx: *mut i32, scale: *mut i32, status: *mut i32) -> i32;
+        
+        //https://github.com/Schwarzam/astrors/blob/2c67c1f76f3cf658f1811d773b87602f05e42573/old/src/main.rs
+        // Is a working example of how to use the fits_hcompress and fits_hdecompress functions
+    
+    
+    }
 }
--- tests/compressed_im_test.rs
+++ tests/compressed_im_test.rs
@@ -27,3 +27,72 @@ fn test_read_fits() -> Result<()> {
     // read_tableinfo_from_header()
     Ok(())
 }
+
+#[test]
+fn compress_random_image() -> Result<()> {
+    
+
+    // let mut rng = rand::thread_rng();
+    // let mut data = Array::from_shape_fn((1000, 1000), |_| rng.gen::<f32>());
+    // let compressed = compress_image(data);
+
+    // read_tableinfo_from_header()
+    Ok(())
+}
+
+
+#[cfg(test)]
+mod test_compression_alg {
+    use std::ffi::CString;
+    use std::os::raw::c_char;
+    use std::any::type_name;
+    use std::mem;
+
+    use rand::{self, Rng};
+    use astrors::io::compression_algorithms::*;
+
+    #[test]
+    fn hcompress() {
+        // create random vec of 300 elements random ints 0 - 100 range
+        let mut rng = rand::thread_rng();
+        let mut a = Vec::with_capacity(300);
+        for _ in 0..300 {
+            a.push(rng.gen_range(0..100));
+        }
+
+        println!("a = {:?}", a.len());
+        let _tamanho = a.len() as i32;
+    
+        let mut nbytes : i64 = 300 * 4;
+        let mut status : i32 = 0;
+        // let mut output : char = String::new();
+    
+        let mut output_buffer: Vec<u8> = vec![0; nbytes as usize];
+        // let output_ptr = output_buffer.as_mut_ptr() as *mut c_char;
+        // std::mem::forget(output_buffer); // Prevent buffer from being deallocated
+        println!("a = {:?}", a);
+
+        let mut scale : i32 = 0;
+        unsafe {
+            fits_hcompress(a.as_mut_ptr().cast(), 300, 1, scale, output_buffer.as_mut_ptr().cast(), &mut nbytes, &mut status);
+        }
+        
+        println!("nbytes = {:?}", nbytes);
+        
+        // let output_string = unsafe { CString::from_raw(output_ptr) };
+        // let output_string = output_string.to_str().expect("Invalid UTF-8");
+        // println!("output_string = {:?}", output_string);
+
+        let mut b = [0; 300];
+        let mut output_buffer2: Vec<u8> = output_buffer[0..nbytes as usize].to_vec();
+
+        unsafe {
+            fits_hdecompress(output_buffer2.as_mut_ptr().cast(), 0, b.as_mut_ptr().cast(), &mut 300, &mut 1, &mut scale, &mut status);
+        }
+        
+        println!("b = {:?}", b);
+            
+        }
+
+    
+}
